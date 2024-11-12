ad7060e8a563f1585598d027b5fa59fa0ce86782
kdemchyshyn <kdemchyshyn@kse.org.ua>
2024-11-12 17:48:56 +0200

    added medal func

--- main.py
+++ main.py
@@ -1,9 +1,39 @@
 import argparse
-import csv
 
-# def medals():
-#     pass
-#
+def medals(team, year):
+
+    medals_list = {'Gold': 0, 'Silver': 0, 'Bronze': 0}
+
+    with open("Olympic Athletes - athlete_events.tsv", "r") as file:
+        header = file.readline().rstrip('\n').split('\t')
+
+        YEAR = header.index("Year")
+        TEAM = header.index("Team")
+        NOC = header.index("NOC")
+        NAME = header.index("Name")
+        EVENT = header.index("Event")
+        MEDAL = header.index("Medal")
+
+        next_line = file.readline()
+        next_line = next_line.rstrip('\n')
+        next_line = next_line.split('\t')
+
+        while next_line != ['']:
+            counter = 0
+            if (next_line[TEAM] == team or next_line[NOC] == team) and counter <= 10 and next_line[YEAR] == year and next_line[MEDAL] != "NA":
+                name = next_line[NAME]
+                event = next_line[EVENT]
+                medal = next_line[MEDAL]
+
+                medals_list[medal] += 1
+                counter += 1
+                print(name, event, medal)
+                #output()
+
+            next_line = file.readline()
+            next_line = next_line.rstrip('\n')
+            next_line = next_line.split('\t')
+
 # def output():
 #     pass
 #
@@ -23,8 +53,8 @@ parser.add_argument("-medals", nargs=2, help="input Team name and year of olympi
 parser.add_argument("-output", help="Name of the file were output will be saved")
 args = parser.parse_args()
 
-medals = args.medals
+team, year = map(str, args.medals)
 output_file = args.output
-print(args)
-print(medals, output_file)
+medals(team, year)
+
 
