b5e516f0ca1896403fa9773b4a50c1cb0b6601d8
kdemchyshyn <kdemchyshyn@kse.org.ua>
2024-11-12 16:01:41 +0200

    added "-medals" and "-output" arguments without func

--- main.py
+++ main.py
@@ -1,9 +1,30 @@
 import argparse
+import csv
+
+# def medals():
+#     pass
+#
+# def output():
+#     pass
+#
+# def total():
+#     pass
+#
+# def overall():
+#     pass
+#
+# def interactive():
+#     pass
 
 # -medals, -output, -total, -overall, -interactive
 parser = argparse.ArgumentParser()
 
-parser.add_argument("medals", help="top-10 athlete in ____ year")
+parser.add_argument("-medals", nargs=2, help="input Team name and year of olympiad")
+parser.add_argument("-output", help="Name of the file were output will be saved")
 args = parser.parse_args()
-print(args.medals)
+
+medals = args.medals
+output_file = args.output
+print(args)
+print(medals, output_file)
 
